fdbd1a1faa5d932d24a165793c6dd20e00bf4986
Bryan Gonzalez <bryan.gonzalez1020.bg@gmail.com>
2019-08-25 16:57:18 -0500

    Info Section Accurate on Home Component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,33 +3,49 @@ import Nav from "../Nav/Nav";
 import HomeHero from "./Hero/Hero";
 import HomeInfo from "./Info/Info";
 
+import axios from "axios";
+
 import "./Home.scss";
 
 class Home extends Component {
   state = {
-    seeMore: false
+    seeMore: false,
+    id: null,
+    house: null
+  };
+
+  componentDidMount = () => {
+    const id = this.props.match.params.id;
+
+    this.setState({ id });
+
+    axios
+      .get(`/api/houses/${id}`)
+      .then(res => this.setState({ house: res.data }))
+      .catch(err => console.log(err));
   };
 
   onSeeMore = () => this.setState({ seeMore: !this.state.seeMore });
 
   onMail = () => alert("Mail");
   onShare = () => alert("Share");
-  render() {
-    const { seeMore } = this.state;
 
-    console.log(seeMore);
+  render() {
+    const { seeMore, id, house } = this.state;
+
+    console.log(id);
 
     return (
       <div className={seeMore ? "Home Scroll" : "Home"}>
         <Nav />
 
-        {/* {seeMore ? <FullHero /> : <HomeHero more={seeMore} />} */}
         <HomeHero more={seeMore} seeMore={this.onSeeMore} />
         <HomeInfo
           more={seeMore}
           seeMore={this.onSeeMore}
           onMail={this.onMail}
           onShare={this.onShare}
+          house={house !== null ? house : "Loading"}
         />
       </div>
     );
--- src/Components/Home/Info/Info.js
+++ src/Components/Home/Info/Info.js
@@ -1,27 +1,38 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
 import Buttons from "./Buttons";
 
 import "./Info.scss";
 
 const HomeInfo = props => {
-  const { seeMore, more, onMail, onShare } = props;
+  const { seeMore, more, onMail, onShare, house } = props;
+  console.log(house);
+
   return (
     <div className={more ? "homeInfo none" : "homeInfo"}>
       <div className="container">
         <div className="name">
-          <h2>Name</h2>
-          <h4>11,200 sqft</h4>
+          <h2>
+            {house.name}{" "}
+            <span style={{ fontSize: "26px" }}>
+              <FontAwesomeIcon icon={faHeart} color="red" />
+              {house.likes}
+            </span>
+          </h2>
+          <h4>{house.size} sqft</h4>
         </div>
 
         <div className="hardInfo">
           <div className="rooms section">
             <h4 className="label">Rooms</h4>
-            <h4 className="value">6</h4>
+            <h4 className="value">{house.rooms}</h4>
           </div>
           <div className="bathRooms section">
             <h4 className="label">Bathrooms</h4>
-            <h4 className="value">9</h4>
+            <h4 className="value">{house.restRooms}</h4>
           </div>
         </div>
 
@@ -32,12 +43,12 @@ const HomeInfo = props => {
 
         <div className="year section">
           <h4 className="label">Year Built</h4>
-          <h4 className="value">2014</h4>
+          <h4 className="value">{house.yearBuilt}</h4>
         </div>
 
         <div className="price section">
           <h4 className="label">Price</h4>
-          <h4 className="value">$ 20M</h4>
+          <h4 className="value">$ {house.price}</h4>
         </div>
 
         <Buttons onMail={onMail} onShare={onShare} seeMore={seeMore} />
