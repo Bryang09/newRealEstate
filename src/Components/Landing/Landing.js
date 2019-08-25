d60203bbc65ee5e07fe70b29e27b9072eae9c940
Bryan Gonzalez <bryan.gonzalez1020.bg@gmail.com>
2019-08-25 12:29:57 -0500

    Front End Proxy Working

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -2,12 +2,20 @@ import React, { Component } from "react";
 import Hero from "./Hero/Hero";
 import Nav from "../Nav/Nav";
 
+import axios from "axios";
+
 class Landing extends Component {
   state = {
     search: false,
     zip: null
   };
 
+  componentDidMount = () => {
+    axios
+      .get("/api/houses")
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
   onZip = e => this.setState({ zip: e.target.value });
 
   render() {
